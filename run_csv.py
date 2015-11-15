c11187b7a3d75e0b3b28011132d3e611e6c93877
excander <excander@mail.ru>
2015-11-15 21:31:29 +0300

    read_inplist() with utf-8

--- run_csv.py
+++ run_csv.py
@@ -1,78 +1,61 @@
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
 from grab import Grab
 import urllib
 import csv
 import time
-from renamer import latinizator
 
 
 def download_csv(word, num):
-	res = urllib.quote(word) 
+    time.sleep(2)
+    res = urllib.quote(word.encode('utf8')) 
 
-	url = "http://zakupki.gov.ru/epz/order/quicksearch/orderCsvSettings/quickSearch/download.html?"+\
-			"placeOfSearch=FZ_44&_placeOfSearch=on&"+\
-			"placeOfSearch=FZ_223&_placeOfSearch=on&"+\
-			"placeOfSearch=FZ_94&_placeOfSearch=on&" +\
-			"priceFrom=0&"+\
-			"priceTo=200+000+000+000&"+\
-			"publishDateFrom=13.11.2015&"+\
-			"publishDateTo=14.11.2015&"+\
-			"updateDateFrom=&"+\
-			"updateDateTo=&"+\
-			"orderStages=AF&_orderStages=on&"+\
-			"orderStages=CA&_orderStages=on&"+\
-			"_orderStages=on&_orderStages=on&"+\
-			"sortDirection=false&"+\
-			"sortBy=UPDATE_DATE&"+\
-			"recordsPerPage=_10&"+\
-			"pageNo=1&"+\
-			"searchString=" + res + "&" +\
-			"strictEqual=false&"+\
-			"morphology=false&"+\
-			"showLotsInfo=false&"+\
-			"isPaging=false&"+\
-			"isHeaderClick=&"+\
-			"checkIds=&"+\
-			"quickSearch=true&"+\
-			"userId=4752519c-eb29-43ba-88e0-8ee913772035&"+\
-			"conf=true;true;true;true;true;true;true;true;false;true;true;true;true;true;true;true;false;"
+    url = "".join([
+    	"http://zakupki.gov.ru/epz/order/quicksearch/orderCsvSettings/quickSearch/download.html?",
+    	"placeOfSearch=FZ_44&_placeOfSearch=on",
+    	"&placeOfSearch=FZ_223&_placeOfSearch=on",
+    	"&placeOfSearch=FZ_94&_placeOfSearch=on",
+    	"&priceFrom=0",
+    	"&priceTo=200+000+000+000",
+    	"&publishDateFrom=13.11.2015",
+    	"&publishDateTo=14.11.2015",
+    	"&updateDateFrom=",
+    	"&updateDateTo=",
+    	"&orderStages=AF&_orderStages=on",
+    	"&orderStages=CA&_orderStages=on",
+    	"&_orderStages=on&_orderStages=on",
+    	"&sortDirection=false",
+    	"&sortBy=UPDATE_DATE",
+    	"&recordsPerPage=_10",
+    	"&pageNo=1",
+    	"&searchString=", res,
+    	"&strictEqual=false",
+    	"&morphology=false",
+    	"&showLotsInfo=false",
+    	"&isPaging=false",
+    	"&isHeaderClick=",
+    	"&checkIds=",
+    	"&quickSearch=true",
+    	"&userId=null",
+    	"&conf=true;true;true;true;true;true;true;true;true;true;true;true;true;true;true;true;true;"])
 
-	g = Grab()
-	g.go(url)
-	g.response.save("static/" + str(num) + " " + latinizator(word) + ".csv")
+    g = Grab()
+    g.go(url)
+    g.response.save("static/" + str(num) + ".csv")
 
 
+def read_inplist():
+    res_line = ''
+    f = open('input.txt', 'rb')
+    for line in f:
+        res_line += line
+    f.close()
+    return unicode(res_line, "utf8").split('\n')
 
-u = 'карандаш'
-inp_list=["уличное освещение",
-			"дорожное освещение",
-			"осветительные приборы",
-			"промышленное освещение",
-			"промышленные светильники",
-			"техническое освещение",
-			"светодиодные технологии",
-			"световой поток",
-			"энергоэффективность",
-			"энергосбережение",
-			"светодиоды",
-			"свет",
-			"источник света",
-			"цветовая температура",
-			"индекс цветопередачи",
-			"светодиод",
-			"вторичная оптика",
-			"корпус светильника",
-			"Реконструкция сетей наружного освещения",
-			"Реконструкция сетей уличного освещения",
-			"Замена осветительного оборудования",
-			"Энергосервисный контракт",
-			"Система уличного освещения",
-			"Система освещения",
-			"Программа энергосбережения",
-			"Программа энергоэффективности"]
+inp_list = read_inplist()
+
 
 for i, inp_word in enumerate(inp_list):
- 	download_csv(inp_word, i)
- 	time.sleep(2)
- 	print "static/" + str(i) + " " + latinizator(inp_word) + ".csv was successfully downloaded"
+    download_csv(inp_word, i)
+    print "static/" + str(i) + ".csv was successfully downloaded"
 
+##print inp_list[0].encode('utf8')
